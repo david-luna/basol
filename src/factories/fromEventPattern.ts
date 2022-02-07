e4b9321c78a40fd69bb1a2227936579d374da4bc
David Luna <luna.bistuer@gmail.com>
2022-02-07 18:38:12 +0100

    feat: add basic implementation of feature

--- src/factories/fromEventPattern.ts
+++ src/factories/fromEventPattern.ts
@@ -1,6 +1,6 @@
 import { Observable } from '../observable';
 
-export type NodeEventHandler = (...args: any[]) => void;
+export type EventHandlerLike = (...args: unknown[]) => void;
 
 /**
  * Creates an Observable from an arbitrary API for registering event handlers.
@@ -11,13 +11,19 @@ export type NodeEventHandler = (...args: any[]) => void;
  * @returns observable
  */
 export function fromEventPattern<T>(
-    addHandler: (handler: NodeEventHandler) => any,
-    removeHandler: (handler: NodeEventHandler, signal: any) => any,
-    resultSelector: (...args: any[]) => T,
+    addHandler: (handler: EventHandlerLike) => any,
+    removeHandler: (handler: EventHandlerLike, signal: any) => any,
+    project?: (...args: any[]) => T,
 ): Observable<T> {
   return new Observable<T>((observer) => {
     // TODO: next function is the actual handler
     // error just throws
     // unsubscribe uses the remove handler function
+    const handler = !project ? observer.next : (...args) => observer.next(project(...args));
+    const signal = addHandler(handler);
+
+    return () => {
+      removeHandler(handler, signal);
+    };
   });
 }
