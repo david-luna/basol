99b43f355f97a11e0a9e16ce5fff51c227f17d26
David Luna <luna.bistuer@gmail.com>
2022-02-07 18:40:26 +0100

    test: tesr skeleton

---
+++ src/factories/fromEventPattern.spec.ts
@@ -0,0 +1,7 @@
+import { fromEventPattern } from './fromEventPattern';
+
+describe('fromEventPattern factory', () => {
+  test('should create an observable which emits when an event occurs', () => {
+    // TODO
+  });
+});
