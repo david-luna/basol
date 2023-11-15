7fbfcf1f106553f0ba1b5bf676f3371f65f99c0a
David Luna <luna.bistuer@gmail.com>
2023-11-15 23:59:00 +0100

    chore: playing with types

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './factories';
-export * from './operators';
-export * from './observable';
-export { pipe } from './utils/pipe';
-export * from './types';
--- src/operators/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './filter';
-export * from './map';
-export * from './mergeMap';
-export * from './switchMap';
-export * from './take';
-export * from './takeWhile';
-export * from './withLatestFrom';
